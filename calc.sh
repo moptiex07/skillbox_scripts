e163c2542b6ae3613b1b3517e6266fd3fe5ade4b
moptiex07 <max.yarkhunin@gmail.com>
2021-08-30 17:41:15 +0000

    Moddified calc.sh Added func sqrt

--- calc.sh
+++ calc.sh
@@ -3,13 +3,17 @@ read -p "enter first value: " x
 read -p "enter second value: " y
 read -p "enter action symbol: " operator
 
+sqrt () {
+	echo sqrt "($1)" | bc -l
+}
+
 case $operator in
 "+") echo $(expr "$x" + "$y");;
 "*") echo $(expr "$x" \* "$y");;
 "-") echo $(expr "$x" - "$y");;
 "/") if [ "$y" -eq 0 ]; then echo "error! no divison on 0!"; else echo $(expr "$x" / "$y"); fi;;
 "**") let q=x**y; echo $q;;
-"sqrt") echo $(echo sqrt "($x)" | bc -l);;
+"sqrt") echo $(sqrt $x) ;;
 *) echo "unknown operation"
 esac
 

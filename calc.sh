4c4d297b74a2b270755a5dd86c20de25c6d2f506
moptiex07 <max.yarkhunin@gmail.com>
2021-08-30 15:01:10 +0000

    Simple calculator with case func

---
+++ calc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+read -p "enter first value: " x
+read -p "enter second value: " y
+read -p "enter action symbol: " operator
+
+case $operator in
+"+") echo $(expr "$x" + "$y");;
+"*") echo $(expr "$x" \* "$y");;
+"-") echo $(expr "$x" - "$y");;
+"/") if [ "$y" -eq 0 ]; then echo "error! no divison on 0!"; else echo $(expr "$x" / "$y"); fi;;
+"**") let q=x**y; echo $q;;
+*) echo "unknown operation"
+esac
+

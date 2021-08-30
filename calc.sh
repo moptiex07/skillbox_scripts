b5c6403c2dba5f8053604e48a4eb8104120fe65e
moptiex07 <max.yarkhunin@gmail.com>
2021-08-30 17:36:47 +0000

    Moddified calc.sh Added sqrt

--- calc.sh
+++ calc.sh
@@ -9,6 +9,7 @@ case $operator in
 "-") echo $(expr "$x" - "$y");;
 "/") if [ "$y" -eq 0 ]; then echo "error! no divison on 0!"; else echo $(expr "$x" / "$y"); fi;;
 "**") let q=x**y; echo $q;;
+"sqrt") echo $(echo sqrt "($x)" | bc -l);;
 *) echo "unknown operation"
 esac
 

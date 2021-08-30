26fce1a7ff5413417599b1dee1bce57421792140
moptiex07 <max.yarkhunin@gmail.com>
2021-08-30 13:05:38 +0000

    Modified mvdir.sh Added check on directory exists or not Added check on stupidness

--- mvdir.sh
+++ mvdir.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
-#copy folder into target folder
-cp -r $1 $2
-#remove old folder
-# -r remove dir
-# -f dont promt, ignore not-existent
-rm -rf $1
+
+if [ $# -ne 2 ]
+then
+	echo "2 arguments are needed, dir being moved and target dir"
+	exit 1
+fi
+
+#check if dir exists
+if [ ! -d "$1" ]
+then
+	echo "Error! $1 does not exist or not a dir"
+	exit 1
+fi
+
+#check if target dir exists
+if [ ! -d "$2" ]
+then
+        echo "Error! $2 does not exist or not a dir"
+	exit 1
+fi
+
+#delete only if dir copied successfully
+if cp -r "$1" "$2" && rm -rf "$1"
+then
+	echo "Dir $1 was successfully moved to $2"
+	exit 0
+else
+	echo "Error! smth went wrong"
+	exit 1
+fi

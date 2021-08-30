186b592c7a78e1b8ea71b90b6d3db0c8deffbeb5
moptiex07 <max.yarkhunin@gmail.com>
2021-08-30 13:14:56 +0000

    Modiffied rename.sh Added files are availiable or not Added check on stupidness

--- rename.sh
+++ rename.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-mv $1 $2
+
+#check 2 arguments
+if [ $# -ne 2 ]
+then
+	echo "2 arguments needed, first name and name after renaming"
+	exit 1
+fi
+
+#check if file exist
+if [ ! -e "$1" ]
+then 
+	echo "Error! $1 does not exist"
+	exit 1
+fi
+
+if mv "$1" "$2"
+then
+	echo "File $1 was successfully renamed to $2"
+	exit 0
+fi
